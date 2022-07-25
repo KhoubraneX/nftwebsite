de6455d18fcba389bf624c14239fd9fa4163d609
MRX <93352394+marounane@users.noreply.github.com>
2022-07-25 07:03:04 +0000

    pop list

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+window.addEventListener("load" , ()=> {
+    let preloder = document.querySelector(".preloder")
+    preloder.style.opacity = "0"
+    setTimeout(()=>{
+        document.querySelector(".preloder").remove()
+        document.body.classList.remove("overflowBody")
+        counterDown(2000)
+    },600)
+})
+
 // counter
 let counterSec = document.querySelectorAll("p.counter span")
 function counterDown(time) {
-    counterSec.forEach((e)=>{
-        let dataGoal =  e.dataset.count
+            counterSec.forEach((e)=>{
+                let dataGoal =  e.dataset.count
         setInterval(()=>{
             if (e.textContent != dataGoal){
                 e.textContent++
@@ -10,13 +20,36 @@ function counterDown(time) {
         },time / dataGoal)
     })
 }
-counterDown(2000)
 
-window.addEventListener("load" , ()=> {
-    let preloder = document.querySelector(".preloder")
-    preloder.style.opacity = "0"
-    setTimeout(()=>{
-        document.querySelector(".preloder").remove()
-    },600)
+// list-pop 
+
+let btnPop = document.querySelector(".list-btn")
+let popList = document.querySelector(".list-pop")
+let popClose = document.querySelector(".list-btn-close")
+
+btnPop.addEventListener("click" , ()=> {
+    popList.setAttribute("style" , "transform: translateX(0);")
+    document.body.classList.add("overflowBody")
+})
+
+popClose.addEventListener("click" , ()=> {
+    popList.setAttribute("style" , "transform: translateX(-100%);")
     document.body.classList.remove("overflowBody")
+})
+
+// to top 
+let btnTop = document.querySelector(".to-top")
+window.addEventListener("scroll" , (e)=>{
+    if (window.scrollY > window.innerHeight) {
+        btnTop.style.display = "block"
+    }else {
+        btnTop.style.display = "none"
+    }
+})
+
+btnTop.addEventListener("click" , ()=> {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    })
 })
